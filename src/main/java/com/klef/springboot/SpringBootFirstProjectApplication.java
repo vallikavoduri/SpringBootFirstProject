805377e7e98073f90df421cf0ec928f060105e28
vallikavoduri <vallikavoduri@gmail.com>
2024-12-06 23:14:56 +0530

    msg

---
+++ src/main/java/com/klef/springboot/SpringBootFirstProjectApplication.java
@@ -0,0 +1,14 @@
+package com.klef.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootFirstProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootFirstProjectApplication.class, args);
+		System.out.println("Hi spring boot this is our first Project ");
+	}
+
+}
